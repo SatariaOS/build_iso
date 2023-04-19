08ea31bd32b7d8974dd8262a5ea8f01a9fa6f6bf
rkarsnk <git-repos-rkarsnk@rkarsnk.jp>
2023-04-19 10:58:36 +0900

    add live-build environmet.

---
+++ livebuild/prep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+lb config \
+	--apt apt \
+	--apt-recommends false \
+	--architecture amd64 \
+	--distribution bookworm  \
+	--parent-distribution bookworm \
+	--parent-debian-installer-distribution bookworm \
+	--archive-areas "main contrib non-free non-free-firmware" \
+	--mirror-bootstrap "http://ftp.riken.go.jp/Linux/debian/debian" \
+	--mirror-chroot "http://ftp.riken.go.jp/Linux/debian/debian" \
+	--mirror-binary "http://ftp.riken.go.jp/Linux/debian/debian" \
+	--bootappend-live "boot=live components debug=1" \
+	--iso-volume "bookworm-live-image"
+
